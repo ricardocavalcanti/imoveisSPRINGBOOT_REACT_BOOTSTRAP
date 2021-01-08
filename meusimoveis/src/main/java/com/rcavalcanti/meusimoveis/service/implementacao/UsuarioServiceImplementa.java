497f60c1ce2a41ab2cfeeb13d678102357e62dd7
ricardo.cavalcanti <ricardocavalcanti@outlook.com>
2021-01-08 02:06:45 -0300

    Criação do serviço de usuário

---
+++ meusimoveis/src/main/java/com/rcavalcanti/meusimoveis/service/implementacao/UsuarioServiceImplementa.java
@@ -0,0 +1,31 @@
+package com.rcavalcanti.meusimoveis.service.implementacao;
+
+import com.rcavalcanti.meusimoveis.model.entity.Usuario;
+import com.rcavalcanti.meusimoveis.model.repository.UsuarioRepository;
+import com.rcavalcanti.meusimoveis.service.UsuarioService;
+
+public class UsuarioServiceImplementa implements UsuarioService {
+	
+	private UsuarioRepository repository;
+	
+	public UsuarioServiceImplementa(UsuarioRepository repository) {
+		super();
+		this.repository = repository;
+	}
+
+	@Override
+	public Usuario autenticar(String email, String senha) {		
+		return null;
+	}
+
+	@Override
+	public Usuario salvarUsuario(Usuario usuario) {		
+		return null;
+	}
+
+	@Override
+	public void validarEmail(String email) {		
+		
+	}
+
+}
