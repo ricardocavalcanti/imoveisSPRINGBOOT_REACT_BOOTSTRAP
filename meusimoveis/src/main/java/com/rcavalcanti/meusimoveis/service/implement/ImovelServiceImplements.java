1519b741d878246db3f22603e304751c0a9e269a
ricardo.cavalcanti <ricardocavalcanti@outlook.com>
2021-01-08 09:43:56 -0300

    Criando serviço Imovel e refatorando

--- meusimoveis/src/main/java/com/rcavalcanti/meusimoveis/service/implement/ImovelServiceImplements.java
+++ meusimoveis/src/main/java/com/rcavalcanti/meusimoveis/service/implement/ImovelServiceImplements.java
@@ -1,5 +1,38 @@
 package com.rcavalcanti.meusimoveis.service.implement;
 
-public class ImovelServiceImplements {
+import java.util.List;
+
+import com.rcavalcanti.meusimoveis.model.entity.Imovel;
+import com.rcavalcanti.meusimoveis.model.enums.StatusImovel;
+import com.rcavalcanti.meusimoveis.service.ImovelService;
+
+public class ImovelServiceImplements implements ImovelService {
+
+	@Override
+	public Imovel salvar(Imovel imovel) {	
+		return null;
+	}
+
+	@Override
+	public Imovel atualizar(Imovel imovel) {		
+		return null;
+	}
+
+	@Override
+	public void deletar(Imovel imovel) {		
+		
+	}
+
+	@Override
+	public List<Imovel> buscar(Imovel imovelFiltro) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void atualizarStatus(Imovel imovelFiltro, StatusImovel statusImovel) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
