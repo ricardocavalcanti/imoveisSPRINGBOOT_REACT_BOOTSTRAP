b6387873eb8cb07b150f27c876100792855d76df
ricardo.cavalcanti <ricardocavalcanti@outlook.com>
2021-01-08 07:20:39 -0300

    Criando o Controller Rest de usuários

---
+++ meusimoveis/src/main/java/com/rcavalcanti/meusimoveis/api/resource/UsuarioResource.java
@@ -0,0 +1,15 @@
+package com.rcavalcanti.meusimoveis.api.resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/usuarios")
+public class UsuarioResource {
+
+//	@GetMapping("/")
+//	public String helloWorld() {
+//		return "Hello World!";
+//	}
+}
